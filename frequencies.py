61a7026162f6a2cc068f2adc80c8d019f0ea5210
JH Park <59407771+zzhoonee@users.noreply.github.com>
2023-09-22 17:52:02 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,12 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    for item in items:
+        key = str(item)
+        frequencies[key] = frequencies.get(key, 0) + 1
+
     return frequencies
